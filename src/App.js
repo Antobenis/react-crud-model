505d05de382e284f6bd50044843860422c4e28c5
Anto <104969793+Antobenis@users.noreply.github.com>
2023-06-15 07:43:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ModalDesign from './modal';
+
+function App() {
+  return (
+    <div>
+      <ModalDesign />
+    </div>
+  );
+}
+
+export default App;
